0700092517e3bae819a7dfb25bf4e2f55f6b62f3
Shiyoii <123533057+Lehnz@users.noreply.github.com>
2023-01-25 06:55:26 +0000

    Developed ― Ping command

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
+const client = new Client({ intents: [
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent
+] });
+
+client.once('ready', (c) => {
+    console.log(`${c.user.tag} is ready to go !`);
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) return;
+
+    switch(message.content.split(' ').shift().toLowerCase()) {
+        case 'ping':
+            message.reply('**Pong!**');
+            break;
+    };
+});
+
+client.login(token);
