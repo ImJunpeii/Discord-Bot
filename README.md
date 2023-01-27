efda58787a569d50ccff20256048ba6908164045
Shiyoii <123533057+Lehnz@users.noreply.github.com>
2023-01-27 19:38:58 +0000

    Added new features

--- README.md
+++ README.md
@@ -3,5 +3,11 @@ Yeah it's a discord bot. Nothing special. I create simple commands with it, and 
 
 # Features
 
-## Ping
+## ;Ping
 A simple ping-pong command. You say ping, it'll reply back saying pong!
+
+## /ping
+Still the simple ping-pong command, but in slash command.
+
+## ;Say
+It's a copycat, it'll copy whatever you say.
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
+require('./commands/index.js')
+
 const client = new Client({ intents: [
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildMembers,
@@ -14,11 +16,21 @@ client.once('ready', (c) => {
 
 client.on('messageCreate', (message) => {
     if (message.author.bot) return;
+    const p = ';'
 
     switch(message.content.split(' ').shift().toLowerCase()) {
-        case 'ping':
+        case `${p}ping`:
             message.reply('**Pong!**');
             break;
+
+        case `${p}say`:
+            if (message.content.split(' ').slice(1) == "") return message.reply({ embeds: [{
+                title: 'Empty message.', color: 0x9971FF
+            }] })
+            message.channel.send(`${message.content.split(/ +/).slice(1)}`)
+            break;
+
+        
     };
 });
 
