ee36b23195e6cf6445f24d995c179511cdf6608e
Shiyoii <123533057+Lehnz@users.noreply.github.com>
2023-01-28 09:40:35 +0000

    New command

--- README.md
+++ README.md
@@ -6,5 +6,8 @@ Yeah it's a discord bot. Nothing special. I create simple commands with it, and 
 ## ;Ping && /ping
 A simple ping-pong command. You say ping, it'll reply back saying pong!
 
-## ;Say && /say (message)
+## ;Say (message) && /say (message)
 It's a copycat, it'll copy whatever you say.
+
+## ;pp [user]
+Shows how long their pp is. Each equal signs represents an inch. The minimum and maximum are ( 0:12 ) in inches. 
--- index.js
+++ index.js
@@ -27,10 +27,19 @@ client.on('messageCreate', (message) => {
             message.channel.send(`${message.content.split(/ +/).slice(1)}`).catch(error => {
                 if (error.code == '50006') {
                     message.reply({ embeds: [{ title: "Error 50006 : Empty message.", color: 0x68FF93 }] })
-                }
-            })
+                };
+            });
             break;
-
+        
+        case 'test':
+            const txt = "=";
+            const num = Math.floor(Math.random() * 12);
+            if (message.mentions.users.size > 0) {
+                const mentionedUser = message.mentions.users.first().username;
+                message.reply({ embeds: [{ title: `${mentionedUser}'s pp size is...`, description: `8${txt.repeat(num)}D`, color: 0x68ff93 }] });
+                return;
+            };
+            message.reply({ embeds: [{ title: "Your pp size is...", description: `8${txt.repeat(num)}D`, color: 0x68ff93 }] });
         
     };
 });
